82714b38417fc99db0801fe90223402da2c0aad5
Mohammed Siddiq <mdsiddiqxen@gmail.com>
2026-04-13 19:12:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -249,7 +249,7 @@ dayFilters.style.display = 'none';
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('./sw.js')
+        navigator.serviceWorker.register('/RP_Schedule/sw.js')
             .then(reg => console.log('Service Worker registered!', reg))
             .catch(err => console.log('Registration failed:', err));
     });
